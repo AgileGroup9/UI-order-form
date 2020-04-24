77ab5055ddf9b5cd0e7772fe9d90f4f3ba926b4a
Joakim Anderlind <joakimanderlind@gmail.com>
2020-04-24 15:54:49 +0200

    Set url to deployment url

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from './js/App';
 
 // Renders the react app into a div in index.html
 var mountNode = document.getElementById('container');
-ReactDOM.render(<App post_address="https://ptsv2.com/t/t9egp-1587724360/post"/>, mountNode);
+ReactDOM.render(<App post_address="/"/>, mountNode);
